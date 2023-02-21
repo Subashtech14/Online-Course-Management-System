dbe92b5fba0e8247d6a378be2af9f85e35f6082d
Subash-Company <Subashtech14@gmail.com>
2023-02-21 16:54:13 +0530

    Course page is completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/course-list/course-list.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+  styleUrls: ['./course-list.component.css']
+})
+
+export class CourseListComponent {
+list=
+[{
+      "Imageurl":"../../assets/java.jfif",
+      "Title":"Java Crash course",
+      "Author":"Subash",
+      "Description":"Learn the java fully ",
+      "Language":"Java"
+    },
+    {
+      "Imageurl":"../../assets/python.png",
+      "Title":"Java Crash course",
+      "Author":"Subash",
+      "Description":"Learn the Python fully ",
+      "Language":"Python"
+    },
+    {
+      "Imageurl":"../../assets/python1.png",
+      "Title":"Java Crash course",
+      "Author":"Subash",
+      "Description":"Learn the master python fully ",
+      "Language":"Python"
+    },
+    {
+      "Imageurl":"../../assets/spring.jfif",
+      "Title":"Java Crash course",
+      "Author":"Subash",
+      "Description":"Learn the java  spring fully ",
+      "Language":"Java"
+    }
+// copy
+,
+{
+  "Imageurl":"../../assets/java.jfif",
+  "Title":"Java Crash course",
+  "Author":"Subash",
+  "Description":"Learn the java fully ",
+  "Language":"Java"
+},
+{
+  "Imageurl":"../../assets/python.png",
+  "Title":"Java Crash course",
+  "Author":"Subash",
+  "Description":"Learn the Python fully ",
+  "Language":"Python"
+},
+{
+  "Imageurl":"../../assets/python1.png",
+  "Title":"Java Crash course",
+  "Author":"Subash",
+  "Description":"Learn the master python fully ",
+  "Language":"Python"
+},
+{
+  "Imageurl":"../../assets/spring.jfif",
+  "Title":"Java Crash course",
+  "Author":"Subash",
+  "Description":"Learn the java  spring fully ",
+  "Language":"Java"
+}
+
+  ]
+
+}
